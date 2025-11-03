8d10b371499471415a0e30c59bca27d344dd4698
momentos <montassar.bendhifallah@gmail.com>
2025-11-03 18:31:15 +0100

    refactor(adapters): separate each repository adapter to its module under repository package

---
+++ infrastructure/adapters/repositories/product_repository_impl.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.ports.repositories.product_repository_port import ProductRepository
+from infrastructure.persistence.models import SQLAlchemyProduct
+
+
+class SQLAlchemyProductAdapter(ProductRepository):
+    def __init__(self, db_url: str):
+        self.engine = create_engine(db_url)
+        self.Session = sessionmaker(bind=self.engine)
+        SQLAlchemyProduct.__table__.create(self.engine)
+    def save(self, product):
+        session = self.Session()
+        db_product = SQLAlchemyProduct.from_domain(product)
+        session.add(db_product)
+        session.commit()
+
+    def get(self, product_id: str):
+        session = self.Session()
+        db_product = session.query(SQLAlchemyProduct).filter(SQLAlchemyProduct.id == product_id).first()
+        return db_product.to_domain() if db_product else None
+
+    def delete(self, product_id: str):
+        session = self.Session()
+        db_product = session.query(SQLAlchemyProduct).filter(SQLAlchemyProduct.id == product_id).first()
+        if db_product:
+            session.delete(db_product)
+            session.commit()
