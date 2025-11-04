8e25e7994d104f953a4f7e29a4b32d637b882034
momentos <montassar.bendhifallah@gmail.com>
2025-11-04 06:45:18 +0100

    refactor(adapeters.repository): remove engine, table and session creation from OrderAdapter

--- infrastructure/adapters/repositories/order_repository_impl.py
+++ infrastructure/adapters/repositories/order_repository_impl.py
@@ -1,30 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 from core.ports.repositories.order_repository_port import OrderRepository
 from infrastructure.persistence.sql.models.order_model import OrderModel
 
 
 class SQLAlchemyOrderAdapter(OrderRepository):
-    def __init__(self, db_url: str):
-        self.engine = create_engine(db_url)
-        self.Session = sessionmaker(bind=self.engine)
-        OrderModel.__table__.create(self.engine)
+    def __init__(self, db: Session):
+        self.session = db
 
     def save(self, order):
-        session = self.Session()
         db_order = OrderModel.from_domain(order)
-        session.add(db_order)
-        session.commit()
+        self.session.add(db_order)
+        self.session.commit()
 
     def get(self, order_id: str):
-        session = self.Session()
-        db_order = session.query(OrderModel).filter(OrderModel.id == order_id).first()
+        db_order = self.session.query(OrderModel).filter(OrderModel.id == order_id).first()
         return db_order.to_domain() if db_order else None
 
     def delete(self, order_id: str):
-        session = self.Session()
-        db_order = session.query(OrderModel).filter(OrderModel.id == order_id).first()
+        db_order = self.session.query(OrderModel).filter(OrderModel.id == order_id).first()
         if db_order:
-            session.delete(db_order)
-            session.commit()
+            self.session.delete(db_order)
+            self.session.commit()
